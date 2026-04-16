1237a81dc998becd0b524e8c8921acef0aa4213f
khushiagrawalbatch2025-ux <khushi.agrawal.batch2025@sitnagpur.siu.edu.in>
2026-04-16 13:54:22 +0530

    Update code 2.c

--- Value_added_problem/code 2.c
+++ Value_added_problem/code 2.c
@@ -1,40 +1,53 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-bool isValid(string s)
-{
-    stack<char> st;
-    for (char ch : s)
-    {
-        if (ch == '(' || ch == '[' || ch == '{')
-        {
-            st.push(ch);
-        }
-        else
-        {
-            if (st.empty())
-            {
-                return false;
-            }
-            char top = st.top();
-            st.pop();
-            if (ch == ')' && top != '(')
-                return false;
-            if (ch == ']' && top != '[')
-                return false;
-            if (ch == '}' && top != '{')
-                return false;
-        }
-    }
-    return st.empty();
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
 }
 
-int main(){
-    string input;
-    cout<<"Enter the input string:"<<endl;
-    cin>>input;
-    bool ans = isValid(input);
-    string output = ans ? "true" : "false";
-    cout<<output;
+char pop() {
+    if (top == -1) return '\0';
+    return stack[top--];
+}
+
+int isMatchingPair(char open, char close) {
+    if (open == '(' && close == ')') return 1;
+    if (open == '{' && close == '}') return 1;
+    if (open == '[' && close == ']') return 1;
+    return 0;
+}
+
+int isBalanced(char* s) {
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            push(s[i]);
+        } else if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+            if (top == -1 || !isMatchingPair(pop(), s[i])) {
+                return 0;
+            }
+        }
+    }
+    return (top == -1); 
+}
+
+int main() {
+    printf("Name : Khushi Agrawal\n");
+    printf("Section : D1\n");
+    printf("PRN : 25070521208\n");
+    printf("\n");
+    
+    char expr[100];
+    printf("Enter expression: ");
+    scanf("%s", expr);
+    
+    if (isBalanced(expr))
+        printf("true\n");
+    else
+        printf("false\n");
     return 0;
 }
